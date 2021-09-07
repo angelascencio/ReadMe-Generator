a77dd721fa61cdc564376543b0c43004b6f1c60a
Angel Ascencio <aascecnio1234@gmail.com>
2021-09-06 20:52:46 -0700

    added prompts to index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter a brief description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions for users',
+        name: "installation"
+    },
+    {
+        type: 'input',
+        message: 'Enter instructions for usage',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Enter instructions for test',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your project',
+    }
+])
